eea1e5352fc930e2d22d73bd5318786d05451ada
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-06-01 23:38:23 +0900

    memberTableViewCellレイアウト設定

---
+++ kitak_trainingApp01/kitak_trainingApp01/MemberTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemberTableViewCell.swift
+//  kitak_trainingApp01
+//
+//  Created by Kazutomo Kita on 2020/06/01.
+//  Copyright © 2020 Kazutomo Kita. All rights reserved.
+//
+
+import UIKit
+
+class MemberTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
